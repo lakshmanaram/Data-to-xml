f162c3b1cd73faed172c1de3350c525306f05139
lakshmanaram <lakshmanaram.n@gmail.com>
2016-01-06 15:44:41 +0530

    attributes added

---
+++ sampledata.py
@@ -0,0 +1,14 @@
+import xmlclass
+'''
+sample xml data
+<CandidateProfile><ProfileName>Automatically generated Europass CV and xslt from EIfEL</ProfileName>
+<PersonalData><PersonDescriptors><DemographicDescriptors><Nationality>French</Nationality>
+<PrimaryLanguage>fr</PrimaryLanguage>
+</DemographicDescriptors>
+<BiologicalDescriptors><DateOfBirth>1972-07-22</DateOfBirth>
+<GenderCode>1</GenderCode>
+</BiologicalDescriptors>
+</PersonDescriptors>
+</PersonalData>
+</CandidateProfile>
+'''
--- xmlclass.py
+++ xmlclass.py
@@ -6,6 +6,35 @@ class Xmldata:
         # data stored in this instance
         self.elements = []
         # list that stores elements (instances of Xmldata)
+        self.atts = []
+        # list that stores mappings of attributes and their respective keys
+
+    def addatt(self,attname,attval):
+        # input - attribute name and value, then add it to the atts list
+        self.atts.append((attname,attval))
+
+    def delatt(self, attname):
+        # deletes an attribute taking name as input
+        for i in self.atts:
+            if attname == i[0]:
+                self.atts.remove(i)
+                break
+
+    def modifyatt(self,attname,attvalue):
+        # modifies the value for the given attribute name, if not found adds it
+        for i in self.atts:
+            if attname == i[0]:
+                self.atts.remove(i)
+                break
+        self.atts.append((attname,attvalue))
+
+    def getattval(self,attname):
+        # get attribute name
+        for i in self.atts:
+            if attname == i[0]:
+                return i[1]
+        print 'attribute not found'
+        return -1
 
     def addele(self,e):
         #adding an element
@@ -34,3 +63,7 @@ class Xmldata:
     def have_elements(self):
         # whether it has elements
         return self.elements.count()!=0
+
+    def have_attributes(self):
+        # whether it has attributes
+        return self.atts.count()!=0
